ebf4bea5baf84a8ffbaecd5aae8eb3c71fc7c05e
송정화 <s00149s@naver.com>
2021-05-25 10:29:05 +0900

    quiz 풀이 2, 3

--- quiz02_1.py
+++ quiz02_1.py
@@ -1,4 +1,4 @@
-def score_input():
+def q2_1():
     score1 = int(input("점수1을 입력하세요"))
     score2 = int(input("점수2를 입력하세요"))
     if score1 >= 50 and score2 >= 50 and (score1 + score2)/2 >= 60:
@@ -7,7 +7,58 @@ def score_input():
         message = "불합격"
     print(message)
 
+    """
+    score1 = int(input("점수1: "))
+    score2 = int(input("점수2: "))
+    average = (score1 + score2) / 2
+    
+    if score 1>= 50 and score2 >= 50 and\
+        average >= 60:
+        message = "합격"
+        
+    else:
+        message = "불합격"
+    
+    print(message)
+    """
+
+def q2_2():
+    for i in range(2, 10):  # 2 ~ 9 단
+        print(i, "단")
+        for x in range(2,10):
+            print(i, "*", x, "=", i * x)
+
+
+def q2_3(): # 무한 루프 : while 문 (continue, break 연습)
+    balance = 0 # 잔액변수 : while문 밖에 넣어야 누적됨
+    while True:
+        method = input("method: ")
+        method = method.lower()
+        if method == "q":
+            break   # 루프 종료
+        if method != "d" and method != "w":
+            print("?")
+            continue
+
+        # d, w만 남음
+        # 금액 입력
+        amount = int(input("Amount:"))
+
+        if method == "d" :  # 입금
+            balance += amount
+        else:   # 출금
+            balance -= amount
+
+        # balance += amount if method == "d" else -amount
+
+        print("Balance: ", balance)
+
+
+    print("프로그램 종료")
+
 
 
 if __name__ == "__main__":
-    score_input()
+    # q2_1()
+    # q2_2
+    q2_3()
--- quiz03_1.py
+++ quiz03_1.py
@@ -1,5 +1,68 @@
-def quiz03_1():
-    integer = int(input())
-    for i in integer:
-        if isinstance(i, int)
+def q3_1():
+    while True:
+        num = input("수를 입력하세요")
 
+        try:
+            int(num)    # 캐스팅
+        except:
+            print("정수가 아닙니다, 다시 입력하세요")
+        else:   #try 블록에 예외 없을때
+            break
+
+    total = 0
+    to = int(num)
+
+    # for i in range(1, to + 1):  # 1 ~ to
+    #     if i % 3 == 0:
+    #         total += i
+
+    lst = [i for i in range(1, to + 1) if i % 3 == 0]   # 3의 배수 리스트 만들기
+    total = sum(lst)
+
+    print("1부터 {}까지의 3의 배수의 합 = {}".format(to, total))
+
+
+def q3_2(): # isinstance 연습
+    lst = [1, 3.14, 'python', 7, 89.1, 3]
+    # lst_cleaned = []
+    # for item in lst:
+    #     if isinstance(item, (int, float)):
+    #         lst_cleaned.append(item)
+    lst_cleaned = [item for item in lst if isinstance(item, (int, float))]
+    print("cleaned : ", lst_cleaned)
+
+def summary(*args): # 가변인수 연습문제
+    total = sum(args)
+    return total, max(args), min(args), total / len(args)
+
+def q3_3():
+    total, maxval, minval, avg = summary(80, 75, 90, 85, 85)
+    print(total, maxval, minval, avg)
+
+def q3_4():
+    s = """We encourage everyone to contribute to Python. 
+    If you still have questions after reviewing the material
+    in this guide, then the Python Mentors 
+    group is available to help guide new contributors through the process."""
+
+    # wjdwp
+    s = s.replace(",", "").replace(".", "").replace("\n", "").upper()
+    splits = s.split()
+    print("splits : ", splits)
+
+    summary = {}    # 결과 dict
+    for word in splits:
+        if word in summary.keys():  # 이미 키에 있으면
+            summary[word] += 1
+        else:   # 키가 없으면
+            summary[word] = 1
+
+    for key, value in summary.items():  # (key, value) 쌍튜플 반환
+        print("{}:{}".format(key, value))
+
+
+if __name__ == "__main__":
+    # q3_1()
+    # q3_2()
+    # q3_3()
+    q3_4()
